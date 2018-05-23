794fa3685d9e5ff134e5543a9068bbfddd97d936
Frank Duncan <frank@kank.net>
2018-05-23 08:24:11 -0500

    Issue #785.  Add Reviewed Documents report.

---
+++ psm-app/integration-tests/src/test/java/gov/medicaid/features/report/steps/ReviewedDocumentsReportSteps.java
@@ -0,0 +1,31 @@
+package gov.medicaid.features.report.steps;
+
+import gov.medicaid.features.report.ui.ReviewedDocumentsPage;
+
+import net.thucydides.core.annotations.Step;
+
+public class ReviewedDocumentsReportSteps {
+    private ReviewedDocumentsPage reviewedDocumentsPage;
+
+    @Step
+    public void checkOnReviewedDocumentsPage() {
+        reviewedDocumentsPage.checkOnPage();
+    }
+
+    /*
+    @Step
+    public void checkReviewedDocumentsPageHasNARowFor(String month) {
+        reviewedDocumentsPage.checkHasRowFor(month);
+    }*/
+
+    @Step
+    public void checkReviewedDocumentsPageHasRowWithEnrollments(String month, String numDocuments) {
+        reviewedDocumentsPage.checkHasRowFor(month);
+        reviewedDocumentsPage.checkRowHasNumber(month, numDocuments);
+    }
+
+    @Step
+    public void downloadReviewedDocumentsReportCsv() {
+        reviewedDocumentsPage.click$(".downloadReviewedDocuments");
+    }
+}
